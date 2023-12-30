4974316366a565c1810f8873b5c66c65828d18fa
yadamzer <yadamzerterefe09@gmail.com>
2023-12-30 18:44:51 +0100

    update shell.c

--- shell.c
+++ shell.c
@@ -4,87 +4,63 @@
  *
  **/
 
-int main(int argc __attribute__((unused)), char *argv[])
-  {
-    char *buffer = NULL;
-    char *token[MAX_LENGTH] = {NULL};
-    char *envp[] = {NULL};
-    size_t num = 0;
-    int i;
-    int max = 0;
-    int status;
-    pid_t pid;
-    ssize_t read;
-    
-    while (1)
-      {
-	if (buffer != NULL)
-	  {
-	  free(buffer);
-	  buffer = NULL;
-	  }
-	
-	printf("#cisfun$ ");
-	fflush(stdout);
+int main(int argc, char *argv[])
+{
+  char *arg[] = {NULL, NULL};
+  char *envp[] = {NULL};
+  char *buffer = NULL; 
+  size_t size = 0;
+  ssize_t read;
+  int pid, status;
 
-	read = getline(&buffer, &num, stdin);
-	if (read == -1)
-	  {
-	    if (feof(stdin))
-	      {
-		printf("\n");
-		break;
-	      }
-	    else
-	      {
-		perror("getline() fails!!!\n");
-		return (-1);
-	      }
-	  }
-	
-	while (buffer[max] != '\n')
-	  max++;
-	buffer[max] = '\0';
-	
-	pid = fork();
+  while (1)
+    {
+      printf("#cisfun$ ");
+      read = getline(&buffer, &size, stdin);
+      if (read == -1)
+	{
+	  if (feof(stdin))
+	    {
+	      printf("\n");
+	      break;
+	    }
+	  else
+	    {
+	      perror("getline fail!!!\n");
+	      return (-1);
+	    }
+	}
+      buffer[strlen(buffer) - 1] = '\0';
 
-	if (pid < 0)
-	  {
-	    perror("fork error!!!\n");
+      pid = fork();
+
+      if (pid < 0)
+	{
+	  perror("fork gets error!!!\n");
+	  return (-1);
+	}
+      else if (pid == 0)
+	{
+	  
+	  arg[0] = malloc((strlen(buffer) + 1) * sizeof(char));
+	  if (arg[0] == NULL)
 	    return (-1);
-	  }
-	else if (pid == 0)
-	  {
-	    i = 0;
-	token[i] = strtok(buffer, " ");
-	while (token[i])
-	  {
-	    i++;
-	    token[i] = strtok(NULL, " ");
-	  }
-	
-	    if (i > 1)
-	      {
-		printf("%s: No such file or directory\n", argv[0]);
-		return (-1);
-	      }
 
-	    printf("##%s##%s##", token[0], token[1]);
-	    fflush(stdout);
-	    
-	    if (execve(token[0], token, envp) == -1)
-	      {
-		printf("%s: error No such file or directory\n", argv[0]);
-		free(buffer);
-		buffer = NULL;
-		return (-1);
-	      }
-	  }
-	else
-	  {
-	    wait(&status);
-	  }
-      }
+	  strcpy(arg[0], buffer);
+	  
+	  if (execve(buffer, arg, envp) == -1)
+	    {
+	      printf("%s: No such file or direcyory\n", argv[0]);
+	      free(buffer);
+	      free(arg[0]);
+	      return (-1);
+	    }
+	}
+      else
+	{
+	  wait(&status);
+	}
+    }
 
-    return (0);
-  }
+  return (0);
+}
--- shell.h
+++ shell.h
@@ -4,12 +4,11 @@
 #define MAX_LENGTH 100
 
 /* Header files */
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdio.h>
 #include <string.h>
-
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #endif
