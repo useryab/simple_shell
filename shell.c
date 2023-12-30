2c3196f072111f32c2e17d17a79067a2d2ea74e5
yadamzer <yadamzerterefe09@gmail.com>
2023-12-30 21:11:10 +0100

    Task 1 & 2

--- shell.c
+++ shell.c
@@ -2,17 +2,23 @@
 
 /**
  *
- **/
+**/
 
 int main(int argc, char *argv[])
 {
-  char *arg[] = {NULL, NULL};
+  char *arg[MAX_LENGTH];
   char *envp[] = {NULL};
-  char *buffer = NULL; 
+  char *buffer = NULL;
+  char *token;
   size_t size = 0;
   ssize_t read;
-  int pid, status;
+  int pid, status, i;
 
+  if (argc > 1)
+    {
+      printf("%s: 0: Can't open %s\n", argv[0], argv[1]);
+      exit(-1);
+    }
   while (1)
     {
       printf("#cisfun$ ");
@@ -21,6 +27,11 @@ int main(int argc, char *argv[])
 	{
 	  if (feof(stdin))
 	    {
+	      if (buffer != NULL)
+		{
+		  free(buffer);
+		  buffer = NULL;
+		}
 	      printf("\n");
 	      break;
 	    }
@@ -37,28 +48,35 @@ int main(int argc, char *argv[])
       if (pid < 0)
 	{
 	  perror("fork gets error!!!\n");
+	  free(buffer);
 	  return (-1);
 	}
       else if (pid == 0)
 	{
-	  
-	  arg[0] = malloc((strlen(buffer) + 1) * sizeof(char));
-	  if (arg[0] == NULL)
-	    return (-1);
+	  token = strtok(buffer, " ");
+	  i = 0;
+	  while (token != NULL)
+	    {
+	      arg[i++] = token;
+	      token = strtok(NULL, " ");
+	    }
+	  arg[i] = NULL;
 
-	  strcpy(arg[0], buffer);
-	  
-	  if (execve(buffer, arg, envp) == -1)
+	  if (execve(arg[0], arg, envp) == -1)
 	    {
 	      printf("%s: No such file or direcyory\n", argv[0]);
 	      free(buffer);
-	      free(arg[0]);
 	      return (-1);
 	    }
 	}
       else
 	{
 	  wait(&status);
+	  if (buffer != NULL)
+	    {
+	      free(buffer);
+	      buffer = NULL;
+	    }
 	}
     }
 
