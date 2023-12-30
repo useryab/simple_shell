8123807b6dcc64f4365e1e2ca64d73527e747b31
root <root@PROMETHEUS>
2023-12-30 15:04:54 -0800

    merge

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define MAX_LENGTH 100
+
+/* Header files */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif
